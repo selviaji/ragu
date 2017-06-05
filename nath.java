03fc7316f4c1eb4786653abd4673129c9df1c9fc
selviaji <ajithngc@gmail.com>
2017-06-05 14:15:07 +0530

    Create nath.java

---
+++ nath.java
@@ -0,0 +1,9 @@
+public class StringReversal {
+	public static void main(String args[]){
+		String input="ajithkumar";
+		String reverse=new StringBuffer(input).reverse().toString();
+		System.out.println("input string:"+input);
+		System.out.println("reversed string:"+reverse);
+	}
+
+}
